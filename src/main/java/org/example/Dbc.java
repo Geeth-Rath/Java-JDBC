a93f2ae8eae91061b9874ec99663aa5edfeab35d
Geethma Rathnayake <69598695+Geeth-Rath@users.noreply.github.com>
2024-01-01 01:27:05 +0530

    Update Dbc.java
    commited

--- src/main/java/org/example/Dbc.java
+++ src/main/java/org/example/Dbc.java
@@ -1,4 +1,24 @@
 package org.example;
+import java.sql.*;
+
 
 public class Dbc {
+
+    public static void main(String arg[]){
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/jdbc-test");
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("select * from Tree");
+            while(resultSet.next()){
+                System.out.println(resultSet.getString("name"));
+            }
+        }
+         catch (Exception e) {
+           e.printStackTrace();
+        }
+
+
+    }
+
 }
